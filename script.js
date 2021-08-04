11204261eba816bb2f377a49e60cb24cda56dcff
DarioPrazeres <87315735+DarioPrazeres@users.noreply.github.com>
2021-08-04 22:13:31 +0100

    update script.js

--- script.js
+++ script.js
@@ -2,63 +2,66 @@ const computerplay = document.querySelector(".computerplay");
 const yourplay = document.querySelector(".yourplay");
 const you = document.querySelector("#you");
 const pc = document.querySelector("#pc");
+
 const score = document.querySelector("#score");
 const round= document.querySelector("#round");
+var tie = 0;
 
 var victory=0;
 var loss=0;
 var numberplay=0;
 function playerSelected(selection){
-    numberplay++; 
+     
   var computer = computerSelected();  
   changeBack(selection);
   scoreGame(computer, selection);
-  if(numberplay === 5){
+  if(numberplay== 5){
 
       if(victory < loss){
-            round.innerHTML = `Lost the Round`;
-            victory = 0;
-            loss = 0;
-            
+            round.innerHTML = `Lost the Round! Victory: ${victory}, Loss: ${loss}, Tie: ${tie}`;
+           
         }else if(victory > loss){
-            round.innerHTML = `you Win the Round`;
-            victory = 0;
-            loss = 0;
+            round.innerHTML = `you Win the Round! Victory: ${victory}, Loss: ${loss}, Tie: ${tie}`;
+
         }else if(victory === loss){
-            round.innerHTML = `Tie Round! Try again`;
+            round.innerHTML = `Tie Round! Try again! Victory: ${victory}, Loss: ${loss}, Tie: ${tie}`;
+
+        }     
             victory = 0;
             loss = 0;
-        }    
+            numberplay = 0;
+            tie = 0;
   }
 }
 function scoreGame(computer,selection){
-    
+    numberplay++;
     if(computer === selection){
+        tie++;
         score.innerHTML = `Tie`;
     }else if(selection === 'SCISSORS' && computer === 'PAPER'){
-            score.innerHTML = `You Win`;
             victory++;
+            score.innerHTML = `You Win`;            
             you.innerHTML = `you: ${victory}`;
-    }else if(selection === 'ROCK' && computer === 'SCISSORS'){
-        score.innerHTML = `You Win`;
-        victory++;
-        you.innerHTML = `you: ${victory}`;
+    }else if(selection === 'ROCK' && computer === 'SCISSORS'){        
+             victory++;
+             score.innerHTML = `You Win`;
+             you.innerHTML = `you: ${victory}`;
     }else if(selection === 'PAPER' && computer === 'ROCK'){
-        score.innerHTML = `You Win`;
-        victory++;
-        you.innerHTML = `you: ${victory}`;
+            victory++;
+            score.innerHTML = `You Win`;        
+            you.innerHTML = `you: ${victory}`;
     }else if(computer === 'SCISSORS' && selection === 'PAPER' ){
-        score.innerHTML = `You lost`;
-        loss++;
-        pc.innerHTML = `Computer: ${loss}`;
+            loss++;
+            score.innerHTML = `You lost`;        
+            pc.innerHTML = `Computer: ${loss}`;
     }else if(computer === 'ROCK' && selection === 'SCISSORS' ){
-        score.innerHTML = `You lost`;
-        loss++;
-        pc.innerHTML = `Computer: ${loss}`;
+            loss++;
+            score.innerHTML = `You lost`;       
+            pc.innerHTML = `Computer: ${loss}`;
     }else if(computer === 'PAPER' && selection === 'ROCK' ){
-        score.innerHTML = `You lost`;
-        loss++;
-        pc.innerHTML = `Computer: ${loss}`;
+            loss++;
+            score.innerHTML = `You lost`;        
+            pc.innerHTML = `Computer: ${loss}`;
     }
 
 }
